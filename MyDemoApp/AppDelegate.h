4e935a4d5317b8a09e97ce79e1a9d45e0d23b825
krishtc <krishhtc@gmail.com>
2014-01-12 10:22:10 +0530

    Initial Commit

---
+++ MyDemoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDemoApp
+//
+//  Created by Krishantha Jayathilake on 1/12/14.
+//  Copyright (c) 2014 Krishantha Jayathilake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
